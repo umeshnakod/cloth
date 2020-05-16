12c016c433dc8d8d3911635d7c4c6943018e3783
umeshnakod <umeshnakod@gmail.com>
2020-05-16 16:17:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -242,4 +242,5 @@ var server = app.listen(9000, function () {
     var host = server.address().address
     var port = server.address().port
     console.log("Example app listening at http://%s:%s", host, port)
+    res.send("Hello wolrd")
 });
