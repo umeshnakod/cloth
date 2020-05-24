f045f45d6ef82442b2120e9b79f74b16d61cc47d
umeshnakod <umeshnakod@gmail.com>
2020-05-24 20:09:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -256,7 +256,7 @@ app.post('/make_cloth_settlement',function(req,res){
 
 
 
-var server = app.listen(3000, function () {
+var server = app.listen(9000, function () {
     var host = server.address().address
     var port = server.address().port
     console.log("Example app listening at http://%s:%s", host, port)
